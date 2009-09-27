0823d749d3de92197b0cb12f8bc3b238f0efd81f
Stas SUSHKOV <stas@net.utcluj.ro>
2009-09-27 11:00:46 +0300

    Rezolvat diacriticele. (fixes #57)

---
+++ js/init.js
@@ -0,0 +1,36 @@
+// La finișarea încărcării paginii
+$(document).ready(function(){
+    // Încarcă navigarea fancy
+    $.localScroll({
+        target: '#corp',
+        queue:true,
+        hash:true,
+        duration:700
+    });
+    // Încarcă lightbox pentru poze
+    $(".polaroid a").fancybox({
+        imageScale:true,
+        hideOnContentClick:true,
+        centerOnScroll:false,
+        zoomSpeedIn: 300,
+        zoomSpeedOut: 300,
+        overlayShow:false
+    });
+    // Funcția caută și înlocuiește diacriticele „greșite”
+    $.fn.escapeDia = function() {
+        this.each(function() {
+            $(this).html(
+                $(this).html()
+                    .replace(/ș/g,"ş")
+                    .replace(/ț/g,"ţ")
+                    .replace(/Ș/g,"Ş")
+                    .replace(/Ț/g,"Ţ")
+                );
+            });
+        return $(this);
+    }
+    // Trece tot „body”-ul prin funcția de diacriticizare!
+    if(navigator.platform == "Win32" || navigator.platform == "Win64") {
+        $('body').escapeDia();
+    }
+});
